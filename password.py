f5db5829d7d5eb8f77f0bf976ae0efb918906cc7
unknown <sawed__off@rambler/ru>
2021-04-25 19:49:08 +0600

    Tourists

--- password.py
+++ password.py
@@ -1,14 +1,12 @@
 a=input('Введите пароль: ')
  
-
 try:
     c = 10 / len(a)
     d=int(a)
     print('Ваш пароль состоит только из цифр')
 
-
 except ZeroDivisionError:
     print('Вы ввели пустой пароль')
 
 except ValueError: 
-    print('Требования к паролю соблюденыe')   
+    print('Требования к паролю соблюдены')   
